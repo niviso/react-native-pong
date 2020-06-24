35cd47e1f348c26468326aa36b31a8d84c6533b0
niviso <nikki.sollid@gmail.com>
2020-06-24 17:07:43 +0200

    Fixing end screen

--- scenes/ending/ending.js
+++ scenes/ending/ending.js
@@ -1,12 +1,16 @@
-import React,{useContext,useEffect,useState} from 'react';
+import React,{useContext} from 'react';
 import Styles from './styles.scss';
 import { Text, View,Image,TouchableOpacity } from 'react-native';
 import AudioHelper from '../../helpers/AudioHelper';
 import {confirm} from '../../helpers/sounds';
 import { SimpleAnimation } from 'react-native-simple-animations';
 import Engine from '../../helpers/engine';
+import {AppContext} from '../../context/appContext';
+
 export default function Ending(props) {
   const {UpdateSceen} = props;
+  const [state,setState] = useContext(AppContext);
+
   AudioHelper.play("menu-music");
 
   return (
@@ -14,7 +18,7 @@ export default function Ending(props) {
     <TouchableOpacity onPress={() => UpdateSceen('select')} style={Styles.Wrapper}>
     <SimpleAnimation delay={250} duration={2000} fade staticType='zoom'>
 
-    <Text style={Styles.Header}>Player 2 winner</Text>
+    <Text style={Styles.Header}>Player {state.player1.points > state.player2.points ? '1' : '2'} won</Text>
     </SimpleAnimation>
     <View style={Styles.Spacer}></View>
     <SimpleAnimation delay={500} duration={2000} fade staticType='zoom'>
